13f68ae2d0b9d19e5a6d4241d5b32a5f637d6463
paraduola <131593278+paraduola@users.noreply.github.com>
2026-06-15 21:38:26 +0800

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/szm/robocup_g1/eigen-3.4.0/unsupported
+# Build directory: /home/szm/robocup_g1/eigen-3.4.0/build/unsupported
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Eigen")
+subdirs("doc")
+subdirs("test")
